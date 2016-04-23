6b446e0e2f627829b2fe86d52188c3d8852d35f2
Sergio Gambarra da Silva <sergio@gambarra.com.br>
2016-04-23 15:17:09 -0300

    eventoDAO alteracao

--- Produto/Implementacao/Sistema-Evento/src/java/persistencia/EventoDAO.java
+++ Produto/Implementacao/Sistema-Evento/src/java/persistencia/EventoDAO.java
@@ -2,6 +2,8 @@
 package persistencia;
 
 import java.util.ArrayList;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import modelo.Evento;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -16,15 +18,27 @@ public class EventoDAO {
     }
     
     public void incluir(Evento evt) {
-        Transaction t = sessao.beginTransaction();
-        sessao.save(evt);
-        t.commit();
+        if(evt.getData_final().after(evt.getData_inicial()) || evt.getData_final().equals(evt.getData_inicial()) ){
+            Transaction t = sessao.beginTransaction();
+            sessao.save(evt);
+            t.commit();
+        } else {
+            FacesContext.getCurrentInstance().addMessage("incluirEvento",new FacesMessage("Data final do evento inferir a inicial"));
+        }
+        
     }
     
       public void alterar(Evento evt) {
-        Transaction t = sessao.beginTransaction();
-        sessao.update(evt);
-        t.commit();
+        if(evt.getData_final().after(evt.getData_inicial()) || evt.getData_final().equals(evt.getData_inicial()) ){
+            Transaction t = sessao.beginTransaction();
+            sessao.update(evt);
+            t.commit();
+            sessao.flush();
+        } else {
+            FacesContext.getCurrentInstance().addMessage("alterarEvento",new FacesMessage("Data final do evento inferir a inicial"));
+        }
+        
+            
     }
       
       
