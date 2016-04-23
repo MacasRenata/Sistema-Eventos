862af73ca2ba6c5ca433733bda9ab478908a99ea
Sergio Gambarra da Silva <sergio@gambarra.com.br>
2016-04-23 15:16:35 -0300

    siseventosBean alteracao

--- Produto/Implementacao/Sistema-Evento/src/java/backingbeans/SisEventosBean.java
+++ Produto/Implementacao/Sistema-Evento/src/java/backingbeans/SisEventosBean.java
@@ -1,18 +1,22 @@
 package backingbeans;
 
-import excecoes.SisEventoException;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import modelo.Evento;
-import org.primefaces.model.UploadedFile;
 import persistencia.EventoDAO;
 
 
 @ManagedBean
 @SessionScoped
+//Se mantenho escopo de sessão, após visualizar os detalhes do evento, quando vou criar outro evento a tela aparece com os
+//campos preenchidos e ao clicar em criar o evento visualizado anteriormente é alterado.
+//Se uso escopo de requisição não consigo alterar o evento, e recebo a seguinte mensagem: 
+//org.hibernate.StaleStateException: Batch update returned unexpected row count from update [0]; actual row count: 0; expected: 1
+
 public class SisEventosBean {
 
     private Evento evento = new Evento();
@@ -38,41 +42,47 @@ public class SisEventosBean {
         return listaEventos;
     }
 
+ 
     public String incluirEvento() {
         FacesContext context = FacesContext.getCurrentInstance();
         FacesMessage msg;
         evtDao.incluir(evento);
         listaEventos = evtDao.listar();
         msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-                "Evento criado, incluído..., com sucesso!", "");
+                "Evento criado com sucesso!", "");
         evento = new Evento();
         context.addMessage(null, msg);
         return null;
     }
 
+     
     public String consultarEvento(int id) {
+       
         evento = evtDao.carregar(id);//idEvento
         return "consultaEvento";
 
     }
     
-     public String iniciaAlteracaoEvento() {    
-         EventoDAO dao = new EventoDAO();
-     evento = dao.carregar(evento.getId());
-     return "alterarEvento2";
-    }
+    //Para ir para a página de alteração do Evento selecionado à partir de Editar, em detalhes do Evento// 
     
-    public String alterarEvento() {               //alterando o evento com os dados armazenados no banco 
+      public String iniciaAlteracaoEvento(int id) {    
+     evento = evtDao.carregar(id);
+     return "alterarEvento";
+      }
+    
+    public String alterarEvento() {             
         FacesContext context = FacesContext.getCurrentInstance();
         FacesMessage msg;
-        EventoDAO dao = new EventoDAO();
-        dao.alterar(evento);
+        evtDao.alterar(evento);
+        listaEventos = evtDao.listar();
         msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
                 "Evento alterado com sucesso!", "");
+ 
+    
         evento = new Evento();
         context.addMessage(null, msg);
-        return "alterarEvento";
+        return "listarEvento";
+        
     }
     
-  }
-
+}
