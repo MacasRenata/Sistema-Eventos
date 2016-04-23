a86f7c2b7ea305d8309f995dcef8bd148da52408
Sergio Gambarra da Silva <sergio@gambarra.com.br>
2016-04-23 11:53:48 -0300

    Criação do usuarioDAO

---
+++ Produto/Implementacao/Sistema-Evento/src/java/persistencia/UsuarioDAO.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+
+import java.util.ArrayList;
+import modelo.Usuario;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author sergio
+ */
+public class UsuarioDAO {
+        private Session sessao;
+    
+    public UsuarioDAO(){
+        sessao = HibernateUtil.getSessionFactory().openSession();
+    
+    }
+    
+    public void incluir(Usuario usr) {
+        Transaction t = sessao.beginTransaction();
+        sessao.save(usr);
+        t.commit();
+    }
+    
+      public void alterar(Usuario usr) {
+        Transaction t = sessao.beginTransaction();
+        sessao.update(usr);
+        t.commit();
+        sessao.flush();
+            
+    }
+      
+      
+      public ArrayList<Usuario> listar() {
+          return (ArrayList<Usuario>) sessao.createCriteria(Usuario.class).list();
+ }
+
+      public Usuario carregar(int id) {
+          return (Usuario) sessao.get(Usuario.class, id);
+      }
+      
+    
+}
