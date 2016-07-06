4654f8918bf04350e6597f46aedcde3765448ecb
RenaTa Maçãs <renata.macas@gmail.com>
2016-07-06 20:28:18 -0300

    Delete File
    Remover arquivos submetidos em eventos

--- Produto/Implementacao/Sistema-Evento/src/java/backingbeans/SisEventosBean.java
+++ Produto/Implementacao/Sistema-Evento/src/java/backingbeans/SisEventosBean.java
@@ -965,6 +965,19 @@ public class SisEventosBean {
         }
 
     }
+    // rmeover arquivos do diretorio
+    public void removerArquivos(File f) {
+        // Se o arquivo passado for um diretório
+        if (f.isDirectory()) {
+                /* Lista todos os arquivos do diretório em um array 
+                   de objetos File */
+                File[] files = f.listFiles();
+                // Identa a lista (foreach) e deleta um por um
+                for (File file : files) {
+                        file.delete();
+                }
+        }
+}
 
     public ArrayList<Categoria> getListaCategorias() {
         CategoriaDAO dao = new CategoriaDAO();
